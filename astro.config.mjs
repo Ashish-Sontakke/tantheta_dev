04b86de583d56d86745b359fb1eabcedd0c0587f
Ashish Sontakke <ashishsontakke12@gmail.com>
2023-03-31 18:57:00 +0530

    remove opengraph tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://tantheta.dev",
   integrations: [
     tailwind(),
     image({
